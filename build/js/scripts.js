daec7d8c59a4780d2a5584439c44355eacda1ad3
gennadiy1970 <ggm_kharkiv@ukr.net>
2018-08-03 23:32:12 +0300

    fix sidebarCollapse batton - js & view

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,14 +1,17 @@
 'use strict';
 
-var btn = document.getElementById('content');
+var btnWrp = document.getElementById('content');
+var btn = document.getElementById('sidebarCollapse');
 var aside = document.querySelector('.categories-wrp .category');
 var header = document.querySelector('.categories-wrp .category__header');
 var main = document.querySelector('.categories-wrp .category__main');
 
-btn.addEventListener('click', hide, false);
+btnWrp.addEventListener('click', hide, false);
+// btn.addEventListener('click', hide, false)
 
 function hide(e) {
   aside.classList.toggle('hide');
   header.classList.toggle('show');
   main.classList.toggle('show');
+  btn.classList.toggle('active');
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,15 @@
-const btn = document.getElementById('content')
+const btnWrp = document.getElementById('content')
+const btn = document.getElementById('sidebarCollapse')
 const aside = document.querySelector('.categories-wrp .category')
 const header = document.querySelector('.categories-wrp .category__header')
 const main = document.querySelector('.categories-wrp .category__main')
 
-btn.addEventListener('click', hide, false)
+btnWrp.addEventListener('click', hide, false)
+// btn.addEventListener('click', hide, false)
 
 function hide(e) {
   aside.classList.toggle('hide');
   header.classList.toggle('show');
   main.classList.toggle('show');
+  btn.classList.toggle('active');
 }
