be5547f10d87e9d4d0b8106df3d117ac463d1726
gennadiy1970 <ggm_kharkiv@ukr.net>
2018-08-04 21:13:45 +0300

    resonsive header

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,16 +1,29 @@
-'use strict';
+"use strict";
 
-var btnWrp = document.getElementById('content');
-var btn = document.getElementById('sidebarCollapse');
-var aside = document.querySelector('.categories-wrp .category');
-var header = document.querySelector('.categories-wrp .category__header');
-var main = document.querySelector('.categories-wrp .category__main');
+var btnWrp = document.getElementById("content");
+var btn = document.getElementById("sidebarCollapse");
+var aside = document.querySelector(".categories-wrp .category");
+var header = document.querySelector(".categories-wrp .category__header");
+var main = document.querySelector(".categories-wrp .category__main");
+var placehold = document.querySelector(".placeholder-text-change");
+window.addEventListener("resize", setPlaceholder, false);
 
-btnWrp.addEventListener('click', hide, false);
+btnWrp.addEventListener("click", hide, false);
+
+function setPlaceholder() {
+  if (document.documentElement.clientWidth < 860) {
+    placehold.setAttribute("placeholder", "Search products ...");
+  } else if (document.documentElement.clientWidth > 1380) {
+    placehold.setAttribute("placeholder", "Search goods, suppliers and create your request...");
+  } else {
+    placehold.setAttribute("placeholder", "Search goods & suppliers...");
+  }
+}
+setPlaceholder();
 
 function hide(e) {
-  aside.classList.toggle('hide');
-  header.classList.toggle('show');
-  main.classList.toggle('show');
-  btn.classList.toggle('active');
+  aside.classList.toggle("hide");
+  header.classList.toggle("show");
+  main.classList.toggle("show");
+  btn.classList.toggle("active");
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,33 @@
-const btnWrp = document.getElementById('content')
-const btn = document.getElementById('sidebarCollapse')
-const aside = document.querySelector('.categories-wrp .category')
-const header = document.querySelector('.categories-wrp .category__header')
-const main = document.querySelector('.categories-wrp .category__main')
+const btnWrp = document.getElementById("content");
+const btn = document.getElementById("sidebarCollapse");
+const aside = document.querySelector(".categories-wrp .category");
+const header = document.querySelector(".categories-wrp .category__header");
+const main = document.querySelector(".categories-wrp .category__main");
+const placehold = document.querySelector(".placeholder-text-change");
+window.addEventListener("resize", setPlaceholder, false);
 
-btnWrp.addEventListener('click', hide, false)
+btnWrp.addEventListener("click", hide, false);
+
+function setPlaceholder() {
+  if (document.documentElement.clientWidth < 860) {
+    placehold.setAttribute("placeholder", "Search products ...");
+  } else if (document.documentElement.clientWidth > 1380){
+    placehold.setAttribute(
+      "placeholder",
+      "Search goods, suppliers and create your request..."
+    );
+  } else {
+    placehold.setAttribute(
+      "placeholder",
+      "Search goods & suppliers..."
+    );
+  }
+}
+setPlaceholder()
 
 function hide(e) {
-  aside.classList.toggle('hide');
-  header.classList.toggle('show');
-  main.classList.toggle('show');
-  btn.classList.toggle('active');
+  aside.classList.toggle("hide");
+  header.classList.toggle("show");
+  main.classList.toggle("show");
+  btn.classList.toggle("active");
 }
