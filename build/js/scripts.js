0d53e93c7f6306589870d2f851062a834165aeb3
gennadiy1970 <ggm_kharkiv@ukr.net>
2018-08-05 05:04:07 +0300

    beta version ready

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,14 +1,18 @@
 "use strict";
 
-var btnWrp = document.getElementById("content");
+var btnWrpHeader = document.getElementById("content");
+var btnWrp = document.getElementById("category__sort-show");
 var btn = document.getElementById("sidebarCollapse");
 var aside = document.querySelector(".categories-wrp .category");
+var card = document.querySelectorAll(".card");
 var header = document.querySelector(".categories-wrp .category__header");
 var main = document.querySelector(".categories-wrp .category__main");
 var placehold = document.querySelector(".placeholder-text-change");
+var filter = document.querySelector(".category__sort-show");
 window.addEventListener("resize", setPlaceholder, false);
 
 btnWrp.addEventListener("click", hide, false);
+btnWrpHeader.addEventListener("click", hide, false);
 
 function setPlaceholder() {
   if (document.documentElement.clientWidth < 860) {
@@ -26,4 +30,13 @@ function hide(e) {
   header.classList.toggle("show");
   main.classList.toggle("show");
   btn.classList.toggle("active");
+  Array.from(card, function (el) {
+    return el.classList.toggle("card--fullscreen");
+  });
+  if (filter.textContent.toLowerCase() === "hide filters") {
+
+    filter.textContent = "show filters";
+  } else {
+    filter.textContent = "hide filters";
+  }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,16 @@
-const btnWrp = document.getElementById("content");
+const btnWrpHeader = document.getElementById("content");
+const btnWrp = document.getElementById("category__sort-show");
 const btn = document.getElementById("sidebarCollapse");
 const aside = document.querySelector(".categories-wrp .category");
+const card = document.querySelectorAll(".card");
 const header = document.querySelector(".categories-wrp .category__header");
 const main = document.querySelector(".categories-wrp .category__main");
 const placehold = document.querySelector(".placeholder-text-change");
+const filter = document.querySelector(".category__sort-show");
 window.addEventListener("resize", setPlaceholder, false);
 
 btnWrp.addEventListener("click", hide, false);
+btnWrpHeader.addEventListener("click", hide, false);
 
 function setPlaceholder() {
   if (document.documentElement.clientWidth < 860) {
@@ -30,4 +34,11 @@ function hide(e) {
   header.classList.toggle("show");
   main.classList.toggle("show");
   btn.classList.toggle("active");
+  Array.from(card, el => el.classList.toggle("card--fullscreen"))
+  if (filter.textContent.toLowerCase() === "hide filters") {
+    
+    filter.textContent = "show filters";
+  } else {
+    filter.textContent = "hide filters";
+  }
 }
